94043701de89e4f6a63f41ec7df37cc3ad122bbe
vladimirvivien <vladimir.vivien@gmail.com>
2014-02-03 21:20:37 -0500

    first commit

---
+++ hdfstat/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "flag"
+import "log"
+import "github.com/vladimirvivien/gowfs"
+
+var nn   = flag.String("namenode", "localhost:50070", "Namenode address")
+var path = flag.String("path", "/", "File path")
+var user = flag.String("user", "", "HDFS user.")
+
+func main() {
+	flag.Parse()
+
+	fs, err := gofs.NewFileSystem(gofs.Configuration{Addr: *nn, User: *user})
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	checksum, err := fs.GetFileChecksum(gofs.Path{Path: *path})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println ("Checksum for ", *path)
+	fmt.Println (checksum)
+}
